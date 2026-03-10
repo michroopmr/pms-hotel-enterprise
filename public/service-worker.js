b4838874f874260e2891162a21c3eaa8f151d104
michroopmr <michroop.mr@gmail.com>
2026-03-10 09:25:38 -0600

    Fix login company_code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,22 @@
-self.addEventListener("install", e => {
+const CACHE = "molly-pms-v1";
+
+self.addEventListener("install", event => {
  console.log("PWA instalada");
+ self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+ console.log("Service Worker activo");
+
+ event.waitUntil(
+   caches.keys().then(keys=>{
+     return Promise.all(
+       keys
+       .filter(k => k !== CACHE)
+       .map(k => caches.delete(k))
+     );
+   })
+ );
+
+ self.clients.claim();
 });
