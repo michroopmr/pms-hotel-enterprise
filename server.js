fd60702c1e6751adb2b7210a07b13e64809373bf
michroopmr <michroop.mr@gmail.com>
2026-04-17 16:12:38 -0600

    fix cors

--- server.js
+++ server.js
@@ -28,6 +28,18 @@ app.use(cors({
 
 app.options(/.*/, cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.set('trust proxy', true);
 
 app.use(express.json());
